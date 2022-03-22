eef41bf58e3cb0644183aedb24e2d584a4abffad
Vanand Mkrtchyan <vanand.mkrtchyan@mamble.co>
2022-03-22 12:56:19 +0400

    setup tailwind and stylelint

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './{common,components,pages,src,styles}/**/*.{jsx,css,scss}',
+    '!/**/*.test.js*',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

b4460f7b4b71a3b58caca964cd4bb4b4149bb4fe
stepm01 <97764749+stepm01@users.noreply.github.com>
2022-09-16 18:45:42 +0400

    Add files via upload

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import DesignRules from '../components/design_rules'
-import Hero from '../components/hero_section/index'
+import DesignRules from '../components/design_rules/DesignRules'
+import Hero from '../components/hero_section/Hero'
+import ResponsiveDesign from '../components/responsive_design/ResponsiveDesign'
 
 const index = () => {
   return (
     <div>
       <Hero />
       <DesignRules />
+      <ResponsiveDesign/>
     </div>
   )
 }
